6e517450c1d256055c9570afcf82cef652418e33
vaidehi modi <vaidehi.modi@4ibiz.in>
2025-01-03 23:10:36 +0530

    update base folder for vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vinstagram/", // Must match your repo name for githib pages
   plugins: [react()],
   resolve: {
     alias: {
