e66074cdb44c5343442394ae61cb1ac99b17c8cb
vaidehi modi <vaidehi.modi@4ibiz.in>
2025-01-03 22:37:10 +0530

    fix index file routing issue

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../../components/layout";
+import Layout from "../../components/layout/index";
 import ProfilePage from "../../components/Profile/ProfilePage";
 import withAuth from "../../../redux/ProtectedRoute";
 
